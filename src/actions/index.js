48e45c5577fc79c32ee580cbfee02ee2d85ee4a1
wwlls <121048014@qq.com>
2018-11-22 11:30:22 +0800

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+import Utils from '../utils';
+import { Toast } from "antd-mobile";
+import * as test from './actionType';
+
+export function setImg(src) {
+  return {
+    type: test.SET_IMG,
+    payload: src
+  };
+}
+
+export const addNum = (value) => {
+	return {
+		type: test.ADD_NUM,
+		value
+	}
+}
+
+export const removeNum = (value) => {
+	return {
+		type: test.REMOVE_NUM,
+		value
+	}
+}
---
+++ src/containers/test/member.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class Member extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className="page page-Member">
+                <p>member页面</p> 
+             </div>
+        );
+    }
+}
