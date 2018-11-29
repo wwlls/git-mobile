1840c2cd1624a42378c2037c7c4fe0775e784b96
wwlls <121048014@qq.com>
2018-11-29 15:31:07 +0800

    金额格式化、时间格式化公用方法

--- src/containers/test/member.js
+++ src/containers/test/member.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Tools from 'utils/tools';
 
 export default class Member extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            money: 1000000000,
+            time: Tools.isMoment(new Date().getTime(), 1),
         };
     }
 
@@ -15,7 +17,9 @@ export default class Member extends React.Component {
     render() {
         return (
             <div className="page page-Member">
-                <p>member页面</p> 
+                <p>member页面</p>
+                <div>金额格式化：{Tools.isNumeral(this.state.money)}</div>
+                <div>时间格式化：{Tools.isMoment(this.state.time, 0)}<br/>{Tools.isMoment(this.state.time, 1)}<br/>{Tools.isMoment(this.state.time, 2)}</div> 
              </div>
         );
     }
